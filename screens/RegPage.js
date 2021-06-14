4efcdb018daff25a03b96f99f51999012929bf37
Dmitry <adichev17@gmail.com>
2021-06-14 13:44:50 +0300

    fix len reg

--- screens/RegPage.js
+++ screens/RegPage.js
@@ -1,99 +1,80 @@
-import * as React from "react";
-import { useState } from "react";
-import {
-	View,
-	Text,
-	TextInput,
-	StyleSheet,
-	CheckBox,
-	TouchableOpacity,
-} from "react-native";
+import * as React from 'react';
+import { useState } from 'react';
+import { View, Text, TextInput, StyleSheet, CheckBox, TouchableOpacity } from 'react-native';
 
 export default function RegScreen({ navigation }) {
-	return (
-		<View>
-			<Text
-				style={{
-					textAlign: "center",
-					marginTop: "15%",
-					fontSize: 25,
-					marginBottom: 20,
-				}}
-			>
-				Регистрация
-			</Text>
-			<TextInput
-				placeholder='Имя'
-				maxLength={32}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
-			<TextInput
-				placeholder='Дата Рождения'
-				maxLength={32}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
-			<TextInput
-				placeholder='Email'
-				maxLength={32}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
-			<TextInput
-				placeholder='Телефон'
-				maxLength={32}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
-			<TextInput
-				secureTextEntry={true}
-				placeholder='Пароль'
-				maxLength={30}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
-			<TextInput
-				secureTextEntry={true}
-				placeholder='Повторите пароль'
-				maxLength={32}
-				autoCorrect={false}
-				style={styles.input}
-			></TextInput>
+  return (
+    <View>
+      <Text
+        style={{
+          textAlign: 'center',
+          marginTop: '15%',
+          fontSize: 25,
+          marginBottom: 20,
+        }}>
+        Регистрация
+      </Text>
+      <TextInput
+        placeholder="Имя"
+        maxLength={29}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
+      <TextInput
+        placeholder="Дата Рождения"
+        maxLength={32}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
+      <TextInput
+        placeholder="Email"
+        maxLength={32}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
+      <TextInput
+        placeholder="Телефон"
+        maxLength={32}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Пароль"
+        maxLength={30}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
+      <TextInput
+        secureTextEntry={true}
+        placeholder="Повторите пароль"
+        maxLength={32}
+        autoCorrect={false}
+        style={styles.input}></TextInput>
 
-			<TouchableOpacity
-				onPress={() => {
-					navigation.navigate("SetCode");
-				}}
-				style={styles.buttonReg}
-			>
-				<Text
-					style={{ textAlign: "center", color: "#fff", fontSize: 18 }}
-				>
-					Зарегистрироваться
-				</Text>
-			</TouchableOpacity>
-		</View>
-	);
+      <TouchableOpacity
+        onPress={() => {
+          navigation.navigate('SetCode');
+        }}
+        style={styles.buttonReg}>
+        <Text style={{ textAlign: 'center', color: '#fff', fontSize: 18 }}>Зарегистрироваться</Text>
+      </TouchableOpacity>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	buttonReg: {
-		marginTop: 80,
-		marginHorizontal: "23%",
-		borderRadius: 20,
-		backgroundColor: "#00a86b",
-		paddingVertical: 10,
-	},
-	input: {
-		borderWidth: 2,
-		borderRadius: 14,
-		height: 50,
-		fontSize: 20,
-		marginHorizontal: 30,
-		backgroundColor: "#e0e0e0",
-		borderColor: "#e0e0e0",
-		paddingHorizontal: 20,
-		marginTop: 20,
-	},
+  buttonReg: {
+    marginTop: 80,
+    marginHorizontal: '23%',
+    borderRadius: 20,
+    backgroundColor: '#00a86b',
+    paddingVertical: 10,
+  },
+  input: {
+    borderWidth: 2,
+    borderRadius: 14,
+    height: 50,
+    fontSize: 20,
+    marginHorizontal: 30,
+    backgroundColor: '#e0e0e0',
+    borderColor: '#e0e0e0',
+    paddingHorizontal: 20,
+    marginTop: 20,
+  },
 });
