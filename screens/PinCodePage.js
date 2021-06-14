c56f4e012e1d91664d97654e0bd58fa2159b5d95
denis200 <soundze@mail.ru>
2021-06-14 16:52:41 +0300

    PinCode screen added

---
+++ screens/PinCodePage.js
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { useState } from "react";
+import {
+	View,
+	Text,
+	TextInput,
+	StyleSheet,
+	CheckBox,
+	TouchableOpacity,
+} from "react-native";
+
+export default function PinCodeScreen({ navigation }) {
+	return (
+		<View>
+			<Text>Hello world</Text>
+		</View>
+	);
+}
