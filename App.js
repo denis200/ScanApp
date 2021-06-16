3653ba328e1dbcda527307df4ea37ef84c4ab350
denis200 <soundze@mail.ru>
2021-06-16 11:14:27 +0300

    Half of AsyncStorage mechanizm is ready

--- App.js
+++ App.js
@@ -1,13 +1,60 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { TouchableOpacity } from 'react-native';
-import { StyleSheet, Text, View } from 'react-native';
-import MainNav from './navigators/MainNavigator'
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { TouchableOpacity, ActivityIndicator } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+
+import { useState } from "react";
+import MainNav from "./navigators/MainNavigator";
+import BottomNavScreen from "./navigators/BottomNav";
+import { AuthContex } from "./components/contex";
 
 export default function App() {
-  return (
-    <MainNav></MainNav>
-  );
+	const [isLoading, setIsLoading] = useState(true);
+	const [token, setToken] = useState(null);
+
+	const authContex = React.useMemo(
+		() => ({
+			signIn: () => {
+				setToken("qwer");
+				setIsLoading(false);
+			},
+			signOut: () => {
+				setToken(null);
+				setIsLoading(false);
+			},
+			signUp: () => {
+				setToken("qwer");
+				setIsLoading(false);
+			},
+		}),
+		[]
+	);
+
+	React.useEffect(() => {
+		setTimeout(() => {
+			setIsLoading(false);
+		}, 1000);
+	}, []);
+
+	if (isLoading) {
+		return (
+			<View
+				style={{
+					flex: 1,
+					justifyContent: "center",
+					alignItems: "center",
+				}}
+			>
+				<ActivityIndicator color='#00aa00' size='large' />
+			</View>
+		);
+	}
+	return (
+		<AuthContex.Provider value={authContex}>
+			<NavigationContainer>
+				{token !== null ? <BottomNavScreen /> : <MainNav />}
+			</NavigationContainer>
+		</AuthContex.Provider>
+	);
 }
-
-
--- screens/ProfilePage.js
+++ screens/ProfilePage.js
@@ -1,146 +1,191 @@
-import * as React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity } from 'react-native';
-import { Button, Image } from 'react-native-elements';
-import { Avatar } from 'react-native-elements';
-import Ionicons from 'react-native-vector-icons/Ionicons';
+import * as React from "react";
+import { StyleSheet, View, Text, TouchableOpacity } from "react-native";
+import { Button, Image } from "react-native-elements";
+import { Avatar } from "react-native-elements";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { AuthContex } from "../components/contex";
 
 export default function ProfileScreen({ navigation }) {
-  return (
-    <View style={{ backgroundColor: '#fff', height: '100%' }}>
-      <View style={{ marginTop: 40 }}>
-        <View style={{ flexDirection: 'row', marginHorizontal: 10 }}>
-          <View style={{ marginLeft: 10 }}>
-            <Avatar
-              size="large"
-              rounded
-              title={'D'}
-              titleStyle={{ color: '#000' }}
-              onPress={() => console.log('Works!')}
-              activeOpacity={0.7}
-              avatarStyle={{
-                borderColor: '#00aa00',
-                borderWidth: 2,
-              }}
-            />
-          </View>
-          <View>
-            <Text style={{ fontSize: 40, marginHorizontal: 20, marginTop: 10 }}>
-              Denis <Ionicons name={'pencil-sharp'} size={25} color={'gray'} />
-            </Text>
-          </View>
-        </View>
-        {/* <View style={{ marginTop: 20 }}>
-          <PersonalUserData />
-        </View>
-        <View>
-          <AdvertisingAndPromotions />
-        </View> */}
-        <View style={{ marginHorizontal: 30 }}>
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Настройки');
-            }}
-            style={{ flexDirection: 'row', marginTop: 50 }}>
-            <Text style={{ fontSize: 30 }}>Настройки профиля</Text>
-            <Ionicons
-              style={{ marginHorizontal: '10%', paddingTop: 1 }}
-              name={'arrow-forward-circle-outline'}
-              size={35}
-              color={'gray'}
-            />
-          </TouchableOpacity>
-          <Image style={{ height: 3, width: '100%' }} source={require('.././src/stick.jpg')} />
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Адреса');
-            }}
-            style={{ flexDirection: 'row', marginTop: 60, justifyContent: 'flex-end' }}>
-            <Text style={{ fontSize: 30, marginRight: '36%' }}>Наши адреса</Text>
-            <Ionicons
-              style={{ marginRight: 9 }}
-              name={'arrow-forward-circle-outline'}
-              size={35}
-              color={'gray'}
-            />
-          </TouchableOpacity>
-          <Image style={{ height: 3, width: '100%' }} source={require('.././src/stick.jpg')} />
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Банковские карты');
-            }}
-            style={{ flexDirection: 'row', marginTop: 60, justifyContent: 'flex-end' }}>
-            <Text style={{ fontSize: 30, marginRight: '17%' }}>Банковские карты</Text>
-            <Ionicons
-              style={{ marginRight: 9 }}
-              name={'arrow-forward-circle-outline'}
-              size={35}
-              color={'gray'}
-            />
-          </TouchableOpacity>
-          <Image style={{ height: 3, width: '100%' }} source={require('.././src/stick.jpg')} />
-          <TouchableOpacity
-            onPress={() => {
-              navigation.navigate('Связь с разработчкиком');
-            }}
-            style={{ flexDirection: 'row', marginTop: 60, justifyContent: 'flex-end' }}>
-            <Text style={{ fontSize: 30, marginRight: '20%' }}>Связаться с нами</Text>
-            <Ionicons
-              style={{ marginRight: 9 }}
-              name={'arrow-forward-circle-outline'}
-              size={35}
-              color={'gray'}
-            />
-          </TouchableOpacity>
-          <Image style={{ height: 3, width: '100%' }} source={require('.././src/stick.jpg')} />
-        </View>
-        <View>
-          <View style={{ marginTop: 80 }}>
-            <Button
-              lab
-              title="Выйти из аккаунта"
-              color="#FF0000"
-              style={{
-                marginHorizontal: '23%',
-                borderRadius: 17,
-                backgroundColor: '#00a86b',
-                color: '#FF0000',
-                textAlign: 'center',
-                fontSize: 22,
-              }}
-            />
-          </View>
-        </View>
-      </View>
-    </View>
-  );
+	const { signOut } = React.useContext(AuthContex);
+
+	return (
+		<View style={{ backgroundColor: "#fff", height: "100%" }}>
+			<View style={{ marginTop: 40 }}>
+				<View style={{ flexDirection: "row", marginHorizontal: 10 }}>
+					<View style={{ marginLeft: 10 }}>
+						<Avatar
+							size='large'
+							rounded
+							title={"D"}
+							titleStyle={{ color: "#000" }}
+							onPress={() => console.log("Works!")}
+							activeOpacity={0.7}
+							avatarStyle={{
+								borderColor: "#00aa00",
+								borderWidth: 2,
+							}}
+						/>
+					</View>
+					<View>
+						<Text
+							style={{
+								fontSize: 40,
+								marginHorizontal: 20,
+								marginTop: 10,
+							}}
+						>
+							Denis{" "}
+							<Ionicons
+								name={"pencil-sharp"}
+								size={25}
+								color={"gray"}
+							/>
+						</Text>
+					</View>
+				</View>
+				<View style={{ marginHorizontal: 30 }}>
+					<TouchableOpacity
+						onPress={() => {
+							navigation.navigate("Настройки");
+						}}
+						style={{ flexDirection: "row", marginTop: 50 }}
+					>
+						<Text style={{ fontSize: 30 }}>Настройки профиля</Text>
+						<Ionicons
+							style={{ marginHorizontal: "10%", paddingTop: 1 }}
+							name={"arrow-forward-circle-outline"}
+							size={35}
+							color={"gray"}
+						/>
+					</TouchableOpacity>
+					<Image
+						style={{ height: 3, width: "100%" }}
+						source={require(".././src/stick.jpg")}
+					/>
+					<TouchableOpacity
+						onPress={() => {
+							navigation.navigate("Адреса");
+						}}
+						style={{
+							flexDirection: "row",
+							marginTop: 60,
+							justifyContent: "flex-end",
+						}}
+					>
+						<Text style={{ fontSize: 30, marginRight: "36%" }}>
+							Наши адреса
+						</Text>
+						<Ionicons
+							style={{ marginRight: 9 }}
+							name={"arrow-forward-circle-outline"}
+							size={35}
+							color={"gray"}
+						/>
+					</TouchableOpacity>
+					<Image
+						style={{ height: 3, width: "100%" }}
+						source={require(".././src/stick.jpg")}
+					/>
+					<TouchableOpacity
+						onPress={() => {
+							navigation.navigate("Банковские карты");
+						}}
+						style={{
+							flexDirection: "row",
+							marginTop: 60,
+							justifyContent: "flex-end",
+						}}
+					>
+						<Text style={{ fontSize: 30, marginRight: "17%" }}>
+							Банковские карты
+						</Text>
+						<Ionicons
+							style={{ marginRight: 9 }}
+							name={"arrow-forward-circle-outline"}
+							size={35}
+							color={"gray"}
+						/>
+					</TouchableOpacity>
+					<Image
+						style={{ height: 3, width: "100%" }}
+						source={require(".././src/stick.jpg")}
+					/>
+					<TouchableOpacity
+						onPress={() => {
+							navigation.navigate("Связь с разработчкиком");
+						}}
+						style={{
+							flexDirection: "row",
+							marginTop: 60,
+							justifyContent: "flex-end",
+						}}
+					>
+						<Text style={{ fontSize: 30, marginRight: "20%" }}>
+							Связаться с нами
+						</Text>
+						<Ionicons
+							style={{ marginRight: 9 }}
+							name={"arrow-forward-circle-outline"}
+							size={35}
+							color={"gray"}
+						/>
+					</TouchableOpacity>
+					<Image
+						style={{ height: 3, width: "100%" }}
+						source={require(".././src/stick.jpg")}
+					/>
+				</View>
+				<View>
+					<View style={{ marginTop: 80 }}>
+						<Button
+							onPress={() => {
+								signOut();
+							}}
+							lab
+							title='Выйти из аккаунта'
+							color='#FF0000'
+							style={{
+								marginHorizontal: "23%",
+								borderRadius: 17,
+								backgroundColor: "#00a86b",
+								color: "#FF0000",
+								textAlign: "center",
+								fontSize: 22,
+							}}
+						/>
+					</View>
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  inputLogin: {
-    borderRadius: 14,
-    height: 50,
-    fontSize: 20,
-    marginHorizontal: 30,
-    backgroundColor: '#e0e0e0',
-    borderColor: '#e0e0e0',
-    paddingHorizontal: 20,
-    marginTop: 10,
-  },
-  buttonAuth: {
-    marginTop: 40,
-    marginHorizontal: '23%',
-    borderRadius: 17,
-    backgroundColor: '#00a86b',
-  },
-  inputPass: {
-    borderWidth: 2,
-    borderRadius: 14,
-    height: 50,
-    fontSize: 20,
-    marginHorizontal: 30,
-    backgroundColor: '#e0e0e0',
-    borderColor: '#e0e0e0',
-    paddingHorizontal: 20,
-    marginTop: 10,
-  },
+	inputLogin: {
+		borderRadius: 14,
+		height: 50,
+		fontSize: 20,
+		marginHorizontal: 30,
+		backgroundColor: "#e0e0e0",
+		borderColor: "#e0e0e0",
+		paddingHorizontal: 20,
+		marginTop: 10,
+	},
+	buttonAuth: {
+		marginTop: 40,
+		marginHorizontal: "23%",
+		borderRadius: 17,
+		backgroundColor: "#00a86b",
+	},
+	inputPass: {
+		borderWidth: 2,
+		borderRadius: 14,
+		height: 50,
+		fontSize: 20,
+		marginHorizontal: 30,
+		backgroundColor: "#e0e0e0",
+		borderColor: "#e0e0e0",
+		paddingHorizontal: 20,
+		marginTop: 10,
+	},
 });
