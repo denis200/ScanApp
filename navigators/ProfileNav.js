3d03585c63a6ce17519cf91b52218322d9c3ec58
denis200 <soundze@mail.ru>
2021-06-17 14:00:02 +0300

    Added map with markers/Adress page

--- navigators/ProfileNav.js
+++ navigators/ProfileNav.js
@@ -1,16 +1,19 @@
-import React from 'react';
-import { createStackNavigator } from '@react-navigation/stack';
-import ProfileScreen from '../screens/ProfilePage';
-import SettingScreen from '../screens/SettingPage';
+import React from "react"
+import {createStackNavigator} from "@react-navigation/stack"
+import ProfileScreen from "../screens/ProfilePage"
+import SettingScreen from "../screens/SettingPage"
+import AdressScreen from "../screens/AdressMapPage"
 
-const ProfileStack = createStackNavigator();
+const ProfileStack = createStackNavigator()
 
 export default function ProfileStackScreen() {
-  return (
-    <ProfileStack.Navigator initialRouteName="Профиль">
-      <ProfileStack.Screen name="Профиль" component={ProfileScreen} />
-      <ProfileStack.Screen name="Настройки" component={SettingScreen} />
-      {/* <ProfileStack.Screen name="Наши адреса" component={ProfileScreen} /> */}
-    </ProfileStack.Navigator>
-  );
+	return (
+		<ProfileStack.Navigator initialRouteName="Профиль">
+			<ProfileStack.Screen name="Профиль" component={ProfileScreen} />
+			<ProfileStack.Screen name="Настройки" component={SettingScreen} />
+			<ProfileStack.Screen name="Адреса" component={AdressScreen} />
+
+			{/* <ProfileStack.Screen name="Наши адреса" component={ProfileScreen} /> */}
+		</ProfileStack.Navigator>
+	)
 }
